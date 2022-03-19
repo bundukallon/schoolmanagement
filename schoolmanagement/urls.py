2f0eafdc2862ee5c822faf688cc42ca4bd66e9ed
bundukallon <100940011+bundukallon@users.noreply.github.com>
2022-03-18 16:17:31 -1200

    Commit

--- schoolmanagement/urls.py
+++ schoolmanagement/urls.py
@@ -8,9 +8,6 @@ from django.urls import path
 from school import views
 from django.contrib.auth.views import LoginView,LogoutView
 
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view,name=''),
@@ -85,6 +82,6 @@ urlpatterns = [
     path('contactus', views.contactus_view),
     path ('gallary', views.gallary_view),
     path('prospectus', views.prospectus_view),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+        
 ]
